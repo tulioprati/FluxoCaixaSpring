f63e345ed54fd0b8a1a5ee5ab831dd6a1e161125
Tulio <tulio.prati12@gmail.com>
2024-02-16 14:49:07 -0300

    first commit

---
+++ src/main/java/io/github/tuliochamorra/ForkApplication.java
@@ -0,0 +1,14 @@
+package io.github.tuliochamorra;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class fluxo-caixaApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(fluxo-caixaApplication.class, args);
+
+    }
+}
